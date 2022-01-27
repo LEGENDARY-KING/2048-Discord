555ce3aad7653bcb14855ca4c94c789a67193c2b
LEGENDARY_KING <aarushsalgania06@gmail.com>
2022-01-27 23:14:45 +0530

    Added help command

---
+++ slashCommands/help.js
@@ -0,0 +1,25 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const dbobj = require("../database.js")
+const config = require("../config.json")
+const Discord = require("discord.js");
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('SomeName')
+		.setDescription('SomeDescription'),
+	async execute(interaction) {
+		let embed = new Discord.MessageEmbed().setTitle("Help Menu")
+			.setDescription(`This is a bot for the game [2048](https://play2048.co/)
+To start a game type \`/start\`
+Thats it, press the buttons to move the tiles in any direction and reach 2048
+
+You can also configure the grid size using \`/start grid:3-10\`
+
+The bot autosaves after every move so dont worry if you have to go, You can continue instantly
+
+You can also save by pressing the save button and then the slot
+And you can load by \`/ start loadslot: [SlotNumber] or Show\` to show all the options`);
+		let link = new Discord.MessageButton.setURL("https://youtu.be/Ru-RKzMohFs").setLabel("Click here for video tutorial").setType("LINK");
+		message.channel.send({embeds:[embed],components:[link]})
+	},
+};
