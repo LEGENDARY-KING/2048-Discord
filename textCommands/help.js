96aeae85e2c165120b379f99d173ade6b2f6fe7e
LEGENDARY_KING <aarushsalgania06@gmail.com>
2022-01-28 08:56:48 +0530

    added discord server and invite in help command

--- textCommands/help.js
+++ textCommands/help.js
@@ -18,9 +18,12 @@ The bot autosaves after every move so dont worry if you have to go, You can cont
 
 You can also save by pressing the save button and then the slot
 And you can load by \`/ start loadslot: [SlotNumber] or Show\` to show all the options`);
+		And you can load by \`/ start loadslot: [SlotNumber] or Show\` to show all the options`);
 		let link = new Discord.MessageActionRow().setComponents([
-			new Discord.MessageButton().setStyle("LINK").setURL("https://youtu.be/Ru-RKzMohFs").setLabel("Click here for video tutorial")
-			])
+			new Discord.MessageButton().setStyle("LINK").setURL("https://youtu.be/Ru-RKzMohFs").setLabel("Video tutorial"),
+			new Discord.MessageButton().setStyle("LINK").setURL("https://discord.com/api/oauth2/authorize?client_id=592382087797932045&permissions=116736&scope=bot%20applications.commands ").setLabel("Invite the bot!"),
+			new Discord.MessageButton().setStyle("LINK").setURL("https://discord.gg/EKZTYtznav").setLabel("Join support server")
+		])
 		message.reply({ embeds: [embed], components: [link]});
 	},
 };
